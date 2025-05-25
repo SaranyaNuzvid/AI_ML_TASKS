9daa3fc15f598181536af2edf19687cb054e964b
Saranya Pitani <hnssaranyasaranya@gmail.com>
2025-05-25 11:35:20 +0530

    NDV_Code_By_SaranyaS_TaxCalculator
    NA

---
+++ NDV_Code_By_SaranyaS_TaxCalculator/TaxCalculator.py
@@ -0,0 +1,3 @@
+This is Saranya from Rajahmundry studying B.Tech,CSE in rajiv Gandhi Univeristy of Knowledge Technologies,Nuzvid.
+This is the first Assignment in my Summer Internship at NDVTechsys Solutions.
+Thankyou sir!
